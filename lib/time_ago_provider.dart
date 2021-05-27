b77b9bc3966cd8286268d605ed9fc4808426ac09
Andreas Bengtsson <andreas@usego.se>
2021-05-27 11:39:44 +0200

    Swedish & Norwegian added to locale logic

--- lib/time_ago_provider.dart
+++ lib/time_ago_provider.dart
@@ -1,12 +1,13 @@
 library time_ago_provider;
 
+export 'src/languages/arabic.dart';
 export 'src/languages/english.dart';
+export 'src/languages/french.dart';
 export 'src/languages/german.dart';
 export 'src/languages/italian.dart';
+export 'src/languages/norwegian.dart';
 export 'src/languages/polish.dart';
-export 'src/languages/french.dart';
-export 'src/languages/arabic.dart';
-export 'src/languages/turkish.dart';
 export 'src/languages/portuguese.dart';
-
+export 'src/languages/swedish.dart';
+export 'src/languages/turkish.dart';
 export 'src/time_ago_provider.dart';
