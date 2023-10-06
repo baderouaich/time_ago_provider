60c2faf995cbf9c05b36276a292e686f525be0de
Mahdi Pishguy <pishguy@gmail.com>
2023-10-06 08:21:08 +0000

    Update persian.dart
    fix all translated words

--- lib/src/languages/persian.dart
+++ lib/src/languages/persian.dart
@@ -4,11 +4,11 @@ class Persian implements Language {
   @override
   bool shortForm;
 
-  Persian({this.shortForm = false});
+  English({this.shortForm = false});
 
   @override
   String seconds(int seconds) =>
-      shortForm ? '$seconds ثانیه' : '$seconds ثانیه';
+      shortForm ? '$seconds sec' : '$seconds ثانیه';
   @override
   String aboutASecond(int seconds) => shortForm ? '۱ ث' : '۱ ثانیه';
   @override
@@ -18,19 +18,20 @@ class Persian implements Language {
   @override
   String suffixAgo() => shortForm ? '' : 'پیش';
   @override
-  String suffixFromNow() => 'بعد';
+  String suffixFromNow() => shortForm ? '' : 'از آلان';
   @override
-  String lessThanOneMinute(int seconds) => 'چند لحظه';
+  String lessThanOneMinute(int seconds) => shortForm ? 'الان' : 'چند لحظه';
   @override
-  String aboutAMinute(int minutes) => shortForm ? '۱ د' : '۱ دقیقه';
+  String aboutAMinute(int minutes) => shortForm ? '۱ دقیقه' : 'دقیقه';
   @override
-  String minutes(int minutes) => shortForm ? '$minutes د' : '$minutes دقیقه';
+  String minutes(int minutes) =>
+      shortForm ? '$minutes دقیقه' : '$minutes دقیقه';
   @override
-  String aboutAnHour(int minutes) => shortForm ? '~۱ س' : 'نزدیک به یک‌ساعت';
+  String aboutAnHour(int minutes) => shortForm ? '~۱ س' : 'حدود یک ساعت';
   @override
   String hours(int hours) => shortForm ? '$hours س' : '$hours ساعت';
   @override
-  String aDay(int hours) => shortForm ? '1 روز' : 'یک روز';
+  String aDay(int hours) => shortForm ? '~1 ر' : 'یک روز';
   @override
   String days(int days) => shortForm ? '$days ر' : '$days روز';
   @override
@@ -38,7 +39,7 @@ class Persian implements Language {
   @override
   String months(int months) => shortForm ? '$months م' : '$months ماه';
   @override
-  String aboutAYear(int year) => shortForm ? '~۱ سال' : 'یک سال';
+  String aboutAYear(int year) => shortForm ? '~۱ سال' : 'حدود یک سال';
   @override
   String years(int years) => shortForm ? '$years سال' : '$years سال';
   @override
